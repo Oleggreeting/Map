cec6bc421462cecce82f25200e582ffd643ecad2
aleh <mattuhin.oleg@gmail.com>
2022-12-17 15:25:51 +0100

    Initial Commit

---
+++ MapView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapView
+//
+//  Created by Олег Mатюхин on 17/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
